5d9e10775b1f1fe02ebda9c46966b40c70ca5f06
10150399 <heechan1.park@kt.com>
2022-10-26 17:51:50 +0900

    chapter12 - 진행중

--- app/src/main/java/com/bignerdranch/android/criminalIntent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalIntent/CrimeListFragment.kt
@@ -8,14 +8,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewTreeLifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bignerdranch.android.criminalIntent.databinding.FragmentCrimeListBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment : Fragment(){
 
     private val crimeListViewModel: CrimeListViewModel by viewModels()
+    private var job: Job? = null
 
     private var _binding: FragmentCrimeListBinding? = null
     private val binding
@@ -42,6 +49,31 @@ class CrimeListFragment : Fragment(){
         binding.crimeRecyclerView.adapter = adapter
 
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        job = viewLifecycleOwner.lifecycleScope.launch{
+            val crimes = crimeListViewModel.loadCrimes()
+            binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        job?.cancel()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch{
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
+                val crimes = crimeListViewModel.loadCrimes()
+                binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
+            }
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/bignerdranch/android/criminalIntent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalIntent/CrimeListViewModel.kt
@@ -1,14 +1,41 @@
 package com.bignerdranch.android.criminalIntent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.*
+
+private const val TAG = "CrimeListViewModel"
 
 class CrimeListViewModel: ViewModel() {
 
     val crimes = mutableListOf<Crime>()
 
     init {
-        for ( i in 0 until 10){
+        Log.d(TAG, "init starting")
+        viewModelScope.launch {
+            Log.d(TAG, "coroutine launched")
+            delay(5000)
+            for ( i in 0 until 10){
+                val crime = Crime(
+                    id = UUID.randomUUID(),
+                    title = "Crime #$i",
+                    date = Date(),
+                    isSolved = i%2 == 0
+                )
+                crimes += crime
+            }
+            crimes += loadCrimes()
+            Log.d(TAG, "Loading crimes finished")
+        }
+    }
+
+    suspend fun loadCrimes(): List<Crime>{
+        val result = mutableListOf<Crime>()
+        delay(5000)
+        for(i in 0 until 100){
             val crime = Crime(
                 id = UUID.randomUUID(),
                 title = "Crime #$i",
@@ -16,7 +43,8 @@ class CrimeListViewModel: ViewModel() {
                 isSolved = i%2 == 0
             )
 
-            crimes += crime
+            result += crime
         }
+        return result
     }
 }
