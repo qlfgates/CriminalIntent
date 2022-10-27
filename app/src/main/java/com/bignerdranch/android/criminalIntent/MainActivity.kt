8b283d6a2ea6054d09a42a5846afc15f83ef620b
10150399 <heechan1.park@kt.com>
2022-10-28 02:28:06 +0900

    chapter13

--- app/src/main/java/com/bignerdranch/android/criminalIntent/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/criminalIntent/MainActivity.kt
@@ -3,7 +3,7 @@ package com.bignerdranch.android.criminalIntent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//chapter12-complete!
+//chapter13
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
