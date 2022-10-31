5a5f55d725434c179ffb64f76b52d680adc31c30
10150399 <heechan1.park@kt.com>
2022-11-01 00:10:30 +0900

    chapter14 - 완료 캘린더 선택가능

--- app/src/main/java/com/bignerdranch/android/criminalIntent/CrimeDetailFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalIntent/CrimeDetailFragment.kt
@@ -5,8 +5,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -65,9 +67,9 @@ class CrimeDetailFragment : Fragment(){
                     oldCrime.copy(title = text.toString())
                 }
             }
-            crimeDate.apply {
-                isEnabled = false
-            }
+//            crimeDate.apply {
+//                isEnabled = false
+//            }
             crimeSolved.setOnCheckedChangeListener { _, isChecked ->
                 crimeDetailViewModel.updateCrime { oldCrime ->
                     oldCrime.copy(isSolved = isChecked)
@@ -82,6 +84,13 @@ class CrimeDetailFragment : Fragment(){
                 }
             }
         }
+        
+        setFragmentResultListener(DatePickerFragment.REQUEST_KEY_DATE){
+            requestKey, bundle ->  val newDate =
+                bundle.getSerializable(DatePickerFragment.BUNDLE_KEY_DATE) as Date
+            crimeDetailViewModel.updateCrime { it.copy(date = newDate) }
+        }
+
     }
 
     private fun updateUi(crime: Crime) {
