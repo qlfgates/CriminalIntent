0d09f7f20f39ce8af8f186a3bfcc7117eb839e89
10150399 <heechan1.park@kt.com>
2022-11-01 00:13:28 +0900

    chapter14 - 챌린지

--- app/src/main/java/com/bignerdranch/android/criminalIntent/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/criminalIntent/MainActivity.kt
@@ -3,7 +3,7 @@ package com.bignerdranch.android.criminalIntent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//chapter13
+//chapter14_challenge
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
