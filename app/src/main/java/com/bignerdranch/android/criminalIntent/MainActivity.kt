1656b657f6a994084169ca94d2f8fbfafc24b991
10150399 <heechan1.park@kt.com>
2022-11-01 00:12:31 +0900

    chapter15 - 시작

--- app/src/main/java/com/bignerdranch/android/criminalIntent/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/criminalIntent/MainActivity.kt
@@ -3,7 +3,7 @@ package com.bignerdranch.android.criminalIntent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//chapter13
+//chapter15
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
